ab326abc1bd98c75a1fcd65c14557361d6de289b
nothingisfunny <if.the.kids.r.united@gmail.com>
2016-06-27 12:11:47 -0700

    signup login views content

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,34 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "password_security"
+  end
+
+  get '/' do
+  	erb :homepage
+  end
+
+  get '/login' do
+  	if logged_in?
+  		
+  	else
+  		erb :login
+  	end
+  end
+
+  post '/login' do
+  	@user = User.find_by(username: params[:username])
+	if @user && @user.authenticate(params[:password])
+		session[:user_id] = @user.id
+  		
+  	else
+  	 	session[:error] = "Something went wrong"
+  	 	redirect to '/login'
+  	end
+  end
+
+ end
