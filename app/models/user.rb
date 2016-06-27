b906b9aad802fb6c52e8622ce2d3374e689a5de8
nothingisfunny <if.the.kids.r.united@gmail.com>
2016-06-27 13:04:15 -0700

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ActiveRecord::Base
 	has_many :recipes
+	has_secure_password
+	validates_presence_of :username, :email, :password
+
+	def slug
+		self.username.gsub(" ", "-").downcase
+	end
+
+	def self.find_by_slug(slug)
+		name = slug.gsub("-", " ")
+		User.all.detect{|user| user.username == name.downcase}
+	end
+
 end
